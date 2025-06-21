7747ce623fc660144c475c444a52fd1a640c2389
xStarH4ndx <brunotoro1799@gmail.com>
2025-06-21 00:15:23 -0400

    actualizacion

--- src/msInventory/modules/compra.module.ts
+++ src/msInventory/modules/compra.module.ts
@@ -10,7 +10,7 @@ import { CompraResolver } from '../resolver/Compra.resolver';
         name: 'INVENTORY_SERVICE',
         transport: Transport.RMQ,
         options: {
-          urls: ['amqps://wfkwwege:MXhjPUXvCp_hdMeVg_16cIXLW0Hta4rc@gull.rmq.cloudamqp.com/wfkwwege'],
+          urls: ['amqps://wfkwwege:UwGEdBBlBLq2Rex4f0xdN_Xhq-PuBq8s@gull.rmq.cloudamqp.com/wfkwwege'],
           queue: 'msinventory.queue',  // Solo cola directa
           queueOptions: {
             durable: true,
--- src/mspayments/payments.module.ts
+++ src/mspayments/payments.module.ts
@@ -11,7 +11,7 @@ import { PaymentResolver } from "./payments.resolver";
                 name: 'PAYMENTS_SERVICE',
                 transport: Transport.RMQ,
                 options: {
-                    urls: ['amqps://wfkwwege:MXhjPUXvCp_hdMeVg_16cIXLW0Hta4rc@gull.rmq.cloudamqp.com/wfkwwege'],
+                    urls: ['amqps://wfkwwege:UwGEdBBlBLq2Rex4f0xdN_Xhq-PuBq8s@gull.rmq.cloudamqp.com/wfkwwege'],
                     queue: 'mspayments.queue',  // Solo cola directa
                     queueOptions: {
                         durable: true,
